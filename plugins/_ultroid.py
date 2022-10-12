ce4ba95eb76b5e5642d5df4bb871e69a9c9d2293
riizzvbss <anandamahar084@gmail.com>
2022-10-12 13:04:14 +0700

    Update _ultroid.py

--- plugins/_ultroid.py
+++ plugins/_ultroid.py
@@ -14,23 +14,23 @@ from telethon.errors import (
 from . import LOG_CHANNEL, LOGS, Button, asst, eor, get_string, ultroid_cmd
 
 REPOMSG = """
-• **ULTROID USERBOT** •\n
-• Repo - [Click Here](https://github.com/TeamUltroid/Ultroid)
-• Addons - [Click Here](https://github.com/TeamUltroid/UltroidAddons)
-• Support - @UltroidSupportChat
+• **ʀɪᴢᴍɪʟ ᴜꜱᴇʀʙᴏᴛ** •\n
+• Repo - [Click Here](https://github.com/rizzvbss/Rizmil)
+• Addons - [Click Here](https://github.com/riizzvbss/addons)
+• Support - [Click Here](https://t.me/kunthulsupport)
 """
 
 RP_BUTTONS = [
     [
-        Button.url(get_string("bot_3"), "https://github.com/TeamUltroid/Ultroid"),
-        Button.url("Addons", "https://github.com/TeamUltroid/UltroidAddons"),
+        Button.url(get_string("bot_3"), "https://github.com/rizzvbss/Rizmil"),
+        Button.url("Addons", "https://github.com/riizzvbss/addons"),
     ],
-    [Button.url("Support Group", "t.me/UltroidSupportChat")],
+    [Button.url("Support Group", "t.me/kunthulsupport")],
 ]
 
-ULTSTRING = """🎇 **Thanks for Deploying Ultroid Userbot!**
+ULTSTRING = """**Thanks for Deploying Rizmil Userbot!**
 
-• Here, are the Some Basic stuff from, where you can Know, about its Usage."""
+• Di sini, adalah Beberapa hal Dasar dari, di mana Anda dapat Tahu, tentang Penggunaannya."""
 
 
 @ultroid_cmd(
@@ -53,13 +53,13 @@ async def repify(e):
     await e.eor(REPOMSG)
 
 
-@ultroid_cmd(pattern="ultroid$")
+@ultroid_cmd(pattern="rizmil$")
 async def useUltroid(rs):
     button = Button.inline("Start >>", "initft_2")
     msg = await asst.send_message(
         LOG_CHANNEL,
         ULTSTRING,
-        file="https://graph.org/file/54a917cc9dbb94733ea5f.jpg",
+        file="https://graph.org/file/e99dc35f50e69851093fd.jpg",
         buttons=button,
     )
     if not (rs.chat_id == LOG_CHANNEL and rs.client._bot):
